867e8f4093233628b62745813fc07fa9bfd9e4a5
Ekta Dubey <131288765+ektadubey2004@users.noreply.github.com>
2023-06-10 01:25:02 +0530

    Update read_a_file

---
+++ Read_a_file.py
@@ -0,0 +1,11 @@
+file1.close()
+file1 = open('myfile.txt', 'r')
+Lines = file1.readlines()
+
+count = 0
+
+for line in Lines:
+	count += 1
+	print("Line".format(count, line.strip()))
+for i in Lines:
+	print(i)
